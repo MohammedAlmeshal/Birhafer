c3972f563f574e64ec6cd8feef49f45cf5d6fbac
Mohammed <ma.almeshal@gmail.com>
2020-07-30 16:10:52 +0300

    adha

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 var express = require("express"),
-    app     = express(),
-bodyParser  = require("body-parser");
+    app = express(),
+    bodyParser = require("body-parser");
 
 
 
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine" , "ejs")
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs")
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + "/public"));
 
 
 
-app.get("/" , (req,res) =>{
+app.get("/", (req, res) => {
     res.render("home");
-    
+
 })
 
-app.post("/" , (req,res) =>{
-    
-   
-    res.render("eidCard" , { name : req.body.name  })
-    
-    console.log("here")
-    
+app.post("/", (req, res) => {
+
+
+    res.render("eidCard", { name: req.body.name })
+
+
+
 })
 
 
@@ -31,4 +31,4 @@ app.post("/" , (req,res) =>{
 
 const port = process.env.PORT || 5000;
 
-app.listen(port , () => console.log("Sever started on port " + port));
+app.listen(port, () => console.log("Sever started on port " + port));
